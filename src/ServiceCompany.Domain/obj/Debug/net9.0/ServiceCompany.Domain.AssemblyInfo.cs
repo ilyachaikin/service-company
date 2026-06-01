bf91fc870596f0c0430ba2bcb78164c1e1c3361e
ilyachaikin <chaykinilya000@gmail.com>
2026-06-01 22:12:48 +0500

    fix3

--- src/ServiceCompany.Domain/obj/Debug/net9.0/ServiceCompany.Domain.AssemblyInfo.cs
+++ src/ServiceCompany.Domain/obj/Debug/net9.0/ServiceCompany.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceCompany.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d0c8a6f908c1552f1bcef7555add2e7e74f994e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c0e03739630852a7ecf9e5dfe864f2b094478f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceCompany.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceCompany.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
